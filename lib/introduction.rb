8187be379aa200793b9b8b5fa6b449272600696c
Okechukwu O Uneze <unezeo@gmail.com>
2020-10-26 14:23:45 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,11 +6,11 @@ def introduction(name) {
   puts "Hi, my name is #{name}."
 }
 
-def introduction_with_language (name, language) {
+def introduction_with_language(name, language) {
   "Hi, my name is #{name} and I am learning to program in #{language}."
 }
 
-def introduction_with_language_optional  (name, language = "Ruby") {
+def introduction_with_language_optional(name, language = "Ruby") {
   "Hi, my name is #{name} and I am learning to program in #{language}."
 }
 
